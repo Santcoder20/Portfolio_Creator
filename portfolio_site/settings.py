14831b4e323f71be9499a2a988c34df582142a21
A.Santhosh Kumar <126486388+Santcoder20@users.noreply.github.com>
2025-10-05 17:14:16 +0530

    Add files via upload

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -98,5 +98,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'santcoder20@gmail.com'
-EMAIL_HOST_PASSWORD = ('zelm swty rgyr gmcw')   # Use an app password, not your real password!
+EMAIL_HOST_USER = 'your_email@mail.com'
+EMAIL_HOST_PASSWORD = ('your-16-char-app-password')   # Use an app password, by createing app https://myaccount.google.com/apppasswords
